81b27f2fdc9f7401e1efc023eb313251fed535ae
Eric Davis <edavis211@mac.com>
2025-10-30 11:41:43 -0400

    style updates

--- template-parts/resource/resource-contributors.php
+++ template-parts/resource/resource-contributors.php
@@ -4,9 +4,11 @@ $contributors = get_field('contributors'); if( $contributors ):
 <section class="resource-contributors">
   <div class="inner">
     <div class="message"><?=$contributors['message']; ?></div>
-    <a href="<?=$contributors['learn_more']; ?>" class="link">
+    <?php if( !empty($contributors['learn_more']) ): ?>
+    <a href="<?=$contributors['learn_more']['url']; ?>" class="link" target="<?=$contributors['learn_more']['target'] ? $contributors['learn_more']['target'] : '_self'; ?>">
       Learn More
     </a>
+    <?php endif; ?>
   </div>
 </section>
 <?php endif; ?>
